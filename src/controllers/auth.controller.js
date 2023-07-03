5e15d11814be2dd2a9659e9efb17d945fba103e6
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-07-02 23:08:44 -0500

    Added login and logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,4 +30,40 @@ export const register = async (req, res) => {
 	}
 };
 
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const userFound = await User.findOne({ email });
+		if (!userFound) {
+			res.status(400).json({ message: 'user not found' });
+		}
+
+		const isMatch = await bcrypt.compare(password, userFound.password);
+		if (!isMatch)
+			return res.status(400).json({ message: 'Invalid credentials' });
+
+		const token = await createAccessToken({ id: userFound._id });
+		res.cookie('token', token);
+		res.json({
+			message: 'User created successfully',
+		});
+
+		res.json({
+			id: userFound._id,
+			username: userFound.username,
+			email: userFound.email,
+			createdAt: userFound.createdAt,
+			updatedAt: userFound.updatedAt,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+export const logout = (req, res) => {
+	res.cookie('token', '', {
+		expires: new Date(0),
+	});
+	return res.sendStatus(200);
+};
