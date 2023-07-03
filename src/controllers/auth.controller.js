010136bae6a0050c87b895efbc23b33c3ae38ceb
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-07-03 08:26:19 -0500

    Added token validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,39 +31,56 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-	const { email, password } = req.body;
-
 	try {
+		const { email, password } = req.body;
 		const userFound = await User.findOne({ email });
+
 		if (!userFound) {
-			res.status(400).json({ message: 'user not found' });
+			return res.status(400).json({ message: 'user not found' });
 		}
 
 		const isMatch = await bcrypt.compare(password, userFound.password);
 		if (!isMatch)
 			return res.status(400).json({ message: 'Invalid credentials' });
 
-		const token = await createAccessToken({ id: userFound._id });
-		res.cookie('token', token);
-		res.json({
-			message: 'User created successfully',
+		const token = await createAccessToken({
+			id: userFound._id,
+			username: userFound.username,
 		});
+		res.cookie('token', token);
+		/*res.json({
+			message: 'User created successfully',
+		});*/
 
 		res.json({
+			id: userFound._id,
+			username: userFound.username,
+			email: userFound.email,
+		});
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+};
+
+export const logout = async (req, res) => {
+	res.cookie('token', '', {
+		expires: new Date(0),
+	});
+	return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+	const userFound = await User.findById(req.user.id);
+
+	if (!userFound) {
+		return res.status(400).json({ message: 'User not found' });
+	} else {
+		return res.json({
 			id: userFound._id,
 			username: userFound.username,
 			email: userFound.email,
 			createdAt: userFound.createdAt,
 			updatedAt: userFound.updatedAt,
 		});
-	} catch (error) {
-		res.status(500).json({ message: error.message });
 	}
-};
-
-export const logout = (req, res) => {
-	res.cookie('token', '', {
-		expires: new Date(0),
-	});
-	return res.sendStatus(200);
 };
