a4a32ced2bc794e18fe55d3d39aa2a817b23c616
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-08-08 12:02:32 -0500

    Added login config

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,3 +3,5 @@ import axios from 'axios';
 const API = 'http://localhost:4500/api';
 
 export const registerRequest = (user) => axios.post(`${API}/auth/register`, user);
+
+export const loginRequest = (user) => axios.post(`${API}/auth/login`, user);
