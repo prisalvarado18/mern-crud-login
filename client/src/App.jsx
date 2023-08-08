4c4c9a8b6e7650a11c6b4496d80f9a5599c3cef6
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-08-08 12:45:49 -0500

    Added final pages to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,24 @@ import { AuthProvider } from './context/AuthContext';
 
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import TasksPage from './pages/TasksPage';
+import TaskFormPage from './pages/TaskFormPage';
+import ProfilePage from './pages/ProfilePage';
+import HomePage from './pages/HomePage';
 
 function App() {
 	return (
 	<AuthProvider>
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<h1>Home Page</h1>} />
+				<Route path='/' element={<HomePage />} />
 				<Route path='/login' element={<LoginPage />} />
 				<Route path='/register' element={<RegisterPage />} />
-				<Route path='/tasks' element={<h1>Task Page</h1>} />
-				<Route path='/add-task' element={<h1>New Task</h1>} />
-				<Route path='/tasks/:id' element={<h1>Update Task</h1>} />
-				<Route path='/profile' element={<h1>Profile</h1>} />
+				
+				<Route path='/tasks' element={<TasksPage />} />
+				<Route path='/add-task' element={<TaskFormPage />} />
+				<Route path='/tasks/:id' element={<TaskFormPage />} />
+				<Route path='/profile' element={<ProfilePage />} />
 			</Routes>
 		</BrowserRouter>
     </AuthProvider>
