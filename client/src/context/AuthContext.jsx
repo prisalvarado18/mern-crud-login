a999b4e722e9a6b0980c77e778b73a9815f8e72e
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-08-09 12:21:23 -0500

    Added navbar config

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -45,6 +45,12 @@ export const AuthProvider = ({ children }) => {
 		}
 	};
 
+	const logout = () => {
+		Cookies.remove('token');
+		setIsAuthenticated(false);
+		setUser(null);
+	}
+
 	useEffect(() => {
 		if (errors.length > 0) {
 			const timer = setTimeout(() => {
@@ -91,6 +97,7 @@ export const AuthProvider = ({ children }) => {
 			value={{
 				signup,
 				signin,
+				logout,
 				user,
 				isAuthenticated,
 				errors,
